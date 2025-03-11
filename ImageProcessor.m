add41d3770b0851bf82f86ec42e5fb3b7e9649c1
Nguyen Anh Minh <anhminh.nguyen@ace.tamut.edu>
2025-03-10 19:13:02 -0500

    Update ImageProcessor.m

--- ImageProcessor.m
+++ ImageProcessor.m
@@ -17,7 +17,7 @@ classdef ImageProcessor
             imwrite(img, filename);
             disp("Image is saved at: " + pwd)
         end
-        function bayerImage = test(rgbImage, filter, show, rgb, filtersize, ord)
+        function bayerImage = convert2Bayer(rgbImage, filter, show, rgb, filtersize, ord)
             cls = class(rgbImage(:,:,1));
             [ri, ci, t] = size(rgbImage);
             if nargin < 6
@@ -85,51 +85,8 @@ classdef ImageProcessor
                 end
             end
         end
-        function bayerImage = convert2Bayer(rgbImage, filter, show, rgb, filtersize, ord)
-            [ri, ci, t] = size(rgbImage);
-            if nargin < 6
-                ord = 0
-            end
-            if nargin > 4
-                [rf, cf] = size(filter);
-                filter = repmat(filter,ceil(filtersize(1)/rf),ceil(filtersize(2)/cf));
-                filter = filter(1:filtersize(1),1:filtersize(2));
-                if ord == 1
-                    filter = sort(sort(filter,2));
-                end
-            end
-            [rf, cf] = size(filter);
-            cls = class(rgbImage(:,:,1))
-            bayerImage = cast(zeros(ri, ci), cls);
-            if nargin < 4
-                rgb = [true true true];
-            end
-            for k = 1:3
-                if rgb(k)
-                    mask = cast(filter == k, cls);
-                    for i = 1:rf:ri
-                        for j = 1:cf:ci
-                            i_end = min(i + rf - 1, ri);
-                            j_end = min(j + cf - 1, ci);
-                            bayerImage(i:1:i_end, j:1:j_end) = bayerImage(i:i_end, j:j_end) + rgbImage(i:i_end, j:j_end, k) .* mask(1:(i_end-i+1), 1:(j_end-j+1));
-                        end
-                    end
-                end
-            end
-            if nargin < 3
-                show = true;
-            end
-            % Show the Bayer-filtered image
-            if show
-                imshow(bayerImage, []);
-                disp(filter);
-            end
-        end
-        function showImage(img)
-            imshow(img, [])
-        end
         function dimension(img)
-            disp("Dimention: ")
+            disp("Dimension of the object: ")
             disp(size(img))
         end
     end
