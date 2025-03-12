991bbe85d99458f5a85114b4177fe86e5d4500f6
Nguyen Anh Minh <anhminh.nguyen@ace.tamut.edu>
2025-03-12 13:30:53 -0500

    Update test-script.m

--- test-script.m
+++ test-script.m
@@ -1,12 +1,25 @@
+data = [2 4 3 2];
+tmp = ImageProcessor.partitionDecomposition(data);
+disp(tmp);
+return;
+data = [
+    1 0 1;
+    0 0 0;
+    1 1 0
+];
+disp(data);
+data = ImageProcessor.customSorting(data, "rc", [1 0]);
+disp(data);
+disp(ImageProcessor.convertMatrix2Partition(data));
+return;
 filter = [
-    3, 1;
+    1, 2;
     2, 3;
 ];
-img = ImageProcessor.readImage('test.png', false);
-ImageProcessor.dimension(img)
-
-disp(img(1:8,1:8, 1:3));
-
-bayerImage = ImageProcessor.test(img, filter, true, [true true true], [2 2], 2);
-ImageProcessor.saveImage(bayerImage, 'output.png');
-disp(bayerImage(1:8,1:8))
+img = ImageProcessor.readImage("test.png");
+bayerImage = ImageProcessor.convert2Bayer(img, filter, true, [true true true], [4 5], 2, "rcr", [2, 1, 3]);
+rgbImage = ImageProcessor.convertBayer2RGB(bayerImage, filter, true, [true true true], [4 5], 2, "rcr", [2, 1, 3]);
+ImageProcessor.showImage(rgbImage);
+imshow(rgb2gray(img));
+imshow(rgb2gray(rgbImage));
+ImageProcessor.saveImage(rgbImage, "output.png");
